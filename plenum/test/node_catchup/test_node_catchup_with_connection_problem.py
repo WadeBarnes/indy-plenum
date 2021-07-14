c82d9f31f83288b9c7bf4179b6b2b08b2db669ed
Wade Barnes <wade@neoterictech.ca>
2021-07-14 10:08:05 -0700

    Re-enable the tests that were hanging.
    - Note the this is a PoC workaround for the hanging issue.
    
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- plenum/test/node_catchup/test_node_catchup_with_connection_problem.py
+++ plenum/test/node_catchup/test_node_catchup_with_connection_problem.py
@@ -14,13 +14,10 @@ from stp_core.types import HA
 call_count = 0
 
 # ToDo:
-#  - Figure out why:
-#   - test_catchup_with_lost_ledger_status hangs if run 4 times
-#   - test_catchup_with_lost_first_consistency_proofs always hangs on the first iteration
-#   - test_cancel_request_cp_and_ls_after_catchup  always hangs on the first iteration
-#  - https://github.com/hyperledger/indy-plenum/issues/1546
-# @pytest.fixture(scope='function', params=range(1, 5))
-@pytest.fixture(scope='function', params=range(1, 4))
+#  - Need a perminent fix for this (one that upgrades RocksDB to the release supported on focal):
+#    - PoC for dependancy pinning workaround for issues with RocksDB 5.17, proposed here:
+#      - https://github.com/hyperledger/indy-plenum/issues/1546
+@pytest.fixture(scope='function', params=range(1, 5))
 def lost_count(request):
     return request.param
 
@@ -80,7 +77,6 @@ def test_catchup_with_lost_ledger_status(txnPoolNodeSet,
     waitNodeDataEquality(looper, node_to_disconnect, *txnPoolNodeSet,
                          exclude_from_check=['check_last_ordered_3pc_backup'])
     
-@pytest.mark.skip(reason="This test hangs on the first iteration.  Investigation required; https://github.com/hyperledger/indy-plenum/issues/1546.")
 def test_catchup_with_lost_first_consistency_proofs(txnPoolNodeSet,
                                                     looper,
                                                     sdk_pool_handle,
@@ -138,7 +134,6 @@ def test_catchup_with_lost_first_consistency_proofs(txnPoolNodeSet,
                          exclude_from_check=['check_last_ordered_3pc_backup'])
 
 
-@pytest.mark.skip(reason="This test hangs on the first iteration.  Investigation required; https://github.com/hyperledger/indy-plenum/issues/1546.")
 def test_cancel_request_cp_and_ls_after_catchup(txnPoolNodeSet,
                                                 looper,
                                                 sdk_pool_handle,
