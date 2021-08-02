00d5a6533893bcd0186017aa22246a6531d3324c
udosson <r.klemens@yahoo.de>
2021-08-02 18:20:55 +0200

    starting with clean up

--- build-scripts/ubuntu-2004/build-3rd-parties.sh
+++ build-scripts/ubuntu-2004/build-3rd-parties.sh
@@ -96,58 +96,27 @@ build_from_pypi pyzmq 18.1.0 bundled
 
 
 ##### install_requires
-build_from_pypi base58 2.1.0
-##### not referenced
-##### build_from_pypi intervaltree 3.1.0
-build_from_pypi ioflo 2.0.2
-build_from_pypi jsonpickle 2.0.0
-build_from_pypi leveldb 0.201
-build_from_pypi libnacl 1.7.2
-build_from_pypi msgpack 0.5.6
-build_from_pypi orderedset 2.0.3
-build_from_pypi packaging 20.9
-build_from_pypi portalocker 2.1.0
+build_from_pypi base58 
+build_from_pypi importlib_metadata
+build_from_pypi ioflo 
+build_from_pypi jsonpickle 
+build_from_pypi leveldb 
+build_from_pypi libnacl 1.6.1
+build_from_pypi msgpack-python
+build_from_pypi orderedset
+build_from_pypi packaging 
+build_from_pypi portalocker
 build_from_pypi prompt-toolkit 3.0.18
-build_from_pypi psutil 5.6.6
-##### not referenced
-##### build_from_pypi pygments 2.7.4
+build_from_pypi psutil 
 build_from_pypi pympler 0.8
-build_from_pypi python-dateutil 2.8.1
-build_from_pypi python-rocksdb 0.7.0
+build_from_pypi python-dateutil 
+build_from_pypi python-rocksdb
 build_from_pypi python-ursa 0.1.1
 build_from_pypi rlp 0.6.0
-build_from_pypi semver 2.13.0
-build_from_pypi sha3 0.2.1
-build_from_pypi six 1.15.0
+build_from_pypi semver 
+build_from_pypi sha3 
+build_from_pypi six 
 build_from_pypi sortedcontainers 1.5.7
 build_from_pypi ujson 1.33
 
-
-
-# build_from_pypi base58 2.1.0
-# build_from_pypi setuptools 50.3.2
-##### not referenced
-##### build_from_pypi pygments 2.7.4
-##### not referenced
-##### build_from_pypi intervaltree 3.1.0
-
-# build_from_pypi pyparsing 2.4.7
-# build_from_pypi eth-utils 1.10.0
-# build_from_pypi pytest 6.2.2
-# build_from_pypi wcwidth 0.2.5
-
-# build_from_pypi eth-hash 0.3.1
-# build_from_pypi eth-typing 2.2.2
-
-
-
-### pytest dependencies
-# build_from_pypi attrs 20.3.0
-# build_from_pypi iniconfig 1.1.1
-# build_from_pypi pluggy 0.13.1
-# build_from_pypi py 1.10.0
-# build_from_pypi toml 0.10.2
-# build_from_pypi msgpack 0.6.2
-
-# build_from_pypi toolz 0.11.1
 
